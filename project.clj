e6ff108b632649fdca9d173d2ef75efe43536ef0
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-01-23 23:26:16 -0600

    Move source files

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject crypto-challenge "0.1.0-SNAPSHOT"
-  :description "Solutions to the Matasano Crypto Challenge"
+(defproject crypto-experiments "0.1.0-SNAPSHOT"
+  :description "Experiments in cryptography with Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.clojure/data.priority-map "0.0.4"]]
-  :main ^:skip-aot crypto-challenge.core
+  :main ^:skip-aot crypto-experiments.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
